aa41798784169ee1f62cde4200e276c4db095892
csyouk <csyouk@hanmail.net>
2017-04-03 12:57:53 +0900

    writing flyweight pattern

---
+++ src/com/ism/Structural/Flyweight/Line.java
@@ -0,0 +1,7 @@
+package com.ism.Structural.Flyweight;
+
+/**
+ * Created by ism on 17. 4. 3.
+ */
+public class Line {
+}
---
+++ src/com/ism/Structural/Proxy/TestProxy.java
@@ -0,0 +1,7 @@
+package com.ism.Structural.Proxy;
+
+/**
+ * Created by ism on 17. 4. 3.
+ */
+public class TestProxy {
+}
